413ecc4edb91c3241a4c92deeedc8533432a7cf8
emrocode <92260849+emrocode@users.noreply.github.com>
2025-09-04 15:45:03 -0300

    Add vercel badge

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
-import { NavLink } from "react-router";
+import { Link } from "react-router";
 import { useThemeStore } from "@/store";
 import { Button } from "@headlessui/react";
+import { VercelBadge } from "./svgl";
 
 export default function Footer() {
   const { toggleDarkMode } = useThemeStore();
@@ -9,17 +10,25 @@ export default function Footer() {
 
   return (
     <footer className="border-line border-y">
-      <div className="container flex min-h-[var(--nav-h)] flex-col items-center gap-x-2 py-8 text-center text-sm">
+      <div className="container flex min-h-[var(--nav-h)] flex-col items-center py-8 text-center text-sm">
+        <Link
+          className="mb-4 hover:bg-inherit"
+          rel="noopener noreferrer"
+          target="_blank"
+          to="https://vercel.com?utm_source=warani&utm_campaign=oss"
+        >
+          <VercelBadge />
+        </Link>
         <span>Este proyecto utiliza la Licencia MIT.</span>
         <span>
           Creado por&nbsp;
-          <NavLink
+          <Link
             rel="noopener noreferrer"
             target="_blank"
             to="https://github.com/emrocode"
           >
             <span>Emilio Romero</span>
-          </NavLink>
+          </Link>
           &nbsp;{date.getFullYear()}&ndash;present
         </span>
         <Button className="mt-2" id="dTheme" onClick={toggleDarkMode}>
