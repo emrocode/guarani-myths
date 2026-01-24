8601dc640074fdd59b72da6db6af248436d2f1e8
emrocode <92260849+emrocode@users.noreply.github.com>
2026-01-24 16:29:39 -0300

    feat(server): add missing schemas

--- server/src/schemas/env.js
+++ server/src/schemas/env.js
@@ -1,7 +1,19 @@
 export const envSchema = {
   type: "object",
-  required: ["MONGODB_URL"],
+  required: [
+    "MONGODB_URL",
+    "UNKEY_API_ID",
+    "UNKEY_ROOT_KEY",
+    "GITHUB_CLIENT_ID",
+    "GITHUB_CLIENT_SECRET",
+    "BASE_URL",
+  ],
   properties: {
     MONGODB_URL: { type: "string" },
+    UNKEY_API_ID: { type: "string" },
+    UNKEY_ROOT_KEY: { type: "string" },
+    GITHUB_CLIENT_ID: { type: "string" },
+    GITHUB_CLIENT_SECRET: { type: "string" },
+    BASE_URL: { type: "string" },
   },
 };
