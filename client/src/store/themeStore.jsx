418cd689ef48c2c20bb10f006d2d10227b62971b
emrocode <92260849+emrocode@users.noreply.github.com>
2026-02-22 16:07:05 -0300

    fix(client): update theme store

--- client/src/store/themeStore.jsx
+++ client/src/store/themeStore.jsx
@@ -1,20 +1,13 @@
 import { create } from "zustand";
 import Darkify from "darkify-js";
 
-let d = null;
+let d = new Darkify("", {
+  autoMatchTheme: true,
+  useLocalStorage: true,
+  useColorScheme: ["oklch(0.9967 0.0013 106.42)", "oklch(0.173 0.0063 91.68)"],
+});
 
 const useThemeStore = create((set) => {
-  if (!d) {
-    d = new Darkify(null, {
-      autoMatchTheme: true,
-      useLocalStorage: true,
-      useColorScheme: [
-        "oklch(0.9967 0.0013 106.42)",
-        "oklch(0.173 0.0063 91.68)",
-      ],
-    });
-  }
-
   return {
     theme: d.getCurrentTheme(),
     toggleTheme: () => {
